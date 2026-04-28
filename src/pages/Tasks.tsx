4e42894a86ae5e9277547d117e50e500f3d1d8d6
guruprasad4256 <152155635+guruprasad4256@users.noreply.github.com>
2026-04-28 11:20:38 +0530

    fix: resolve typescript build errors and paths

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -6,15 +6,13 @@ import Testimonials from "../components/Testimonials";
 import Footer from "../components/Footer/Footer";
 import TaskHowItWorks from "../components/TaskHowItworks";
 import RecommendedServices from "../components/RecommendedServices";
-// import TaskHero from "../components/Taskhero";
-import Logo from "@/assets/logo.png";
+
 
 const Tasks = () => {
   return (
     <div>
       <Header />
       <TaskHerobg />
-      {/* <TaskHero /> */}
       <TaskHowItWorks />
       <RecommendedServices />
       <GetInTouchCTA />
