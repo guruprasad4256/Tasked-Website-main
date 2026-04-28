8f45ea4cbdd6c9d45236ca08060d5347c7b73864
guruprasad4256 <152155635+guruprasad4256@users.noreply.github.com>
2026-04-28 11:26:21 +0530

    resolved type script error

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,26 +1,26 @@
-import Header from "../components/Header/Header";
-import FAQsHome from "../components/FAQsHome";
-import GetInTouchCTA from "../components/GetInTouchCTA";
-import TaskHerobg from "../components/TaskHerobg";
-import Testimonials from "../components/Testimonials";
-import Footer from "../components/Footer/Footer";
-import TaskHowItWorks from "../components/TaskHowItworks";
-import RecommendedServices from "../components/RecommendedServices";
+// import Header from "../components/Header/Header";
+// import FAQsHome from "../components/FAQsHome";
+// import GetInTouchCTA from "../components/GetInTouchCTA";
+// import TaskHerobg from "../components/TaskHerobg";
+// import Testimonials from "../components/Testimonials";
+// import Footer from "../components/Footer/Footer";
+// import TaskHowItWorks from "../components/TaskHowItworks";
+// import RecommendedServices from "../components/RecommendedServices";
 
 
-const Tasks = () => {
-  return (
-    <div>
-      <Header />
-      <TaskHerobg />
-      <TaskHowItWorks />
-      <RecommendedServices />
-      <GetInTouchCTA />
-      <Testimonials />
-      <FAQsHome />
-      <Footer />
-    </div>
-  );
-};
+// const Tasks = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <TaskHerobg />
+//       <TaskHowItWorks />
+//       <RecommendedServices />
+//       <GetInTouchCTA />
+//       <Testimonials />
+//       <FAQsHome />
+//       <Footer />
+//     </div>
+//   );
+// };
 
-export default Tasks;
+// export default Tasks;
